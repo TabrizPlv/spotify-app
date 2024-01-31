5bab8c4c93efafc0c366f5371d87f3532ceb1a71
TabrizPlv <tabrizpahlavi@gmail.com>
2024-02-01 00:20:13 +0800

    Fix refresh access token bug

--- src/utils/tokens/ValidateTokenRoute.js
+++ src/utils/tokens/ValidateTokenRoute.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { refreshToken } from "../../axios/tokenRequests/refreshToken";
 import isTokenExpired from "./isTokenExpired";
 
-
 const ValidateTokenRoute = ({ children }) => {
   const [validToken, setValidToken] = useState(
     localStorage.getItem("access_token")
@@ -15,11 +14,13 @@ const ValidateTokenRoute = ({ children }) => {
         try {
           console.log("Refreshing Token");
           const old_refresh_token = localStorage.getItem("refresh_token");
-          const { access_token, refresh_token, expires_in } =
-            await refreshToken(old_refresh_token);
+          //No new refresh token is issued
+          const { access_token, expires_in } = await refreshToken(
+            old_refresh_token
+          );
           localStorage.setItem("access_token", access_token);
-          localStorage.setItem("refresh_token", refresh_token);
           localStorage.setItem("expireTime", expires_in);
+          localStorage.setItem("timeStamp", Date.now());
         } catch (e) {
           console.log("Error refreshing token: ", e.message);
           alert("Error: Please login again!");
